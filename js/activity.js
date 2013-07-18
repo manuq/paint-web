981abf353b86626d43849f183ed37171d7aaeea9
Manuel Quiñones <manuq@laptop.org>
2013-07-18 18:55:32 -0300

    Basic painting

--- js/activity.js
+++ js/activity.js
@@ -8,18 +8,70 @@ define(function (require) {
         // Initialize the activity.
         activity.setup();
 
-        //Create a stage by getting a reference to the canvas
-        stage = new createjs.Stage("my-canvas");
-        //Create a Shape DisplayObject.
-        circle = new createjs.Shape();
-        circle.graphics.beginFill("red").drawCircle(0, 0, 40);
-        //Set position of Shape instance.
-        circle.x = circle.y = 50;
-        //Add Shape instance to stage display list.
-        stage.addChild(circle);
-        //Update stage will render next frame
-        stage.update();
+        var paintCanvas = document.getElementById("paint-canvas");
 
+        var updateSizes = function () {
+            var toolbarElem = document.getElementById("main-toolbar");
+            var height = window.innerHeight - toolbarElem.offsetHeight - 5;
+            paintCanvas.width = window.innerWidth - 5;
+            paintCanvas.height = height;
+        }
+        updateSizes();
+
+        window.onresize = function (event) {
+                updateSizes();
+        }
+
+        var stage = new createjs.Stage(paintCanvas);
+
+        //check to see if we are running in a browser with touch support
+        stage.autoClear = false;
+        stage.enableDOMEvents(true);
+        createjs.Touch.enable(stage);
+        createjs.Ticker.setFPS(24);
+
+        stage.addEventListener("stagemousedown", handleMouseDown);
+        stage.addEventListener("stagemouseup", handleMouseUp);
+
+        // set up our defaults:
+        var strokeColor = "#309090";
+        var strokeSize = 40;
+        var shape;
+        var oldPoint;
+        var oldMidPoint;
+
+        var shape = new createjs.Shape();
+        stage.addChild(shape);
+
+        // add handler for stage mouse events:
+        function handleMouseDown(event) {
+            oldPoint = new createjs.Point(stage.mouseX, stage.mouseY);
+            oldMidPoint = oldPoint;
+            stage.addEventListener("stagemousemove" , handleMouseMove);
+        }
+
+        function handleMouseUp(event) {
+            stage.removeEventListener("stagemousemove" , handleMouseMove);
+        }
+
+        function handleMouseMove(event) {
+            var midPoint = new createjs.Point(oldPoint.x + stage.mouseX>>1,
+                                              oldPoint.y+stage.mouseY>>1);
+
+            shape.graphics.clear().setStrokeStyle(strokeSize, 'round', 'round').
+                beginStroke(strokeColor).moveTo(midPoint.x, midPoint.y).
+                curveTo(oldPoint.x, oldPoint.y, oldMidPoint.x, oldMidPoint.y);
+
+            oldPoint.x = stage.mouseX;
+            oldPoint.y = stage.mouseY;
+
+            oldMidPoint.x = midPoint.x;
+            oldMidPoint.y = midPoint.y;
+
+            stage.update();
+        }
+
+        stage.update();
     });
 
 });
